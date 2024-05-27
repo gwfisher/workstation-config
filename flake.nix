27ba22847a2febda0d7b6648526b34425696ca2a
Wes Fisher <wfisher@praetor.tel>
2024-05-27 04:09:59 -0500

    home-manager

--- flake.nix
+++ flake.nix
@@ -5,9 +5,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     microvm.url = "github:astro/microvm.nix";
     microvm.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 };
 
-  outputs = { self, nixpkgs, ...  }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ...  }@inputs: {
     nixosConfigurations = {
       odin = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -15,6 +18,13 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/odin
+
+	  home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.wfisher = import ./users/wfisher/home.nix;
+	  }
         ];
       };
     };
