545006aeac2b91bc63a1f91a8858ad5e1139bae6
Wes Fisher <wfisher@praetor.tel>
2024-06-05 11:29:03 -0500

    sync

--- users/wfisher/home.nix
+++ users/wfisher/home.nix
@@ -9,19 +9,44 @@
 
 	programs.zsh = {
 		enable = true;
+		enableCompletion = true;
+		ohMyZsh = {
+			enable = true;
+      		autosuggestions.enable = true;
+      		zsh-autoenv.enable = true;
+      		syntaxHighlighting.enable = true;
+      		oh-my-zsh = {
+         		enable = true;
+         		theme = "agnoster";
+         		plugins = [
+           			"git"
+           			"history"
+				];
+			};
+		};
+	};
+
+	home.sessionVariables = rec {
+		GPG_TTY="$( tty )";
 	};
 
 	programs.kitty = {
 		enable = true;
+		settings = {
+				font_family = "Inconsolata";
+				font_size = "12";
+		};
+		extraConfig = "include ./nord.conf";
 	};
 	
 	programs.vscode = {
 		enable = true;
 	};
 
+
 	home.packages = with pkgs; [
     	gh
-	    google-chrome
+		google-chrome
 	    gnome3.gnome-tweaks
 		spotify
 		discord
@@ -29,6 +54,8 @@
 		obsidian
 		prismlauncher
 		gimp
+		unzip
+		zstd
 		
 	];
 
